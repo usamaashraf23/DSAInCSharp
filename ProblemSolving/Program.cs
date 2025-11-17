0da851f8f3bb492932db70c89533581d79d5d4fb
Usama Ashraf <usamaashraf47@yahoo.com>
2025-11-17 11:28:22 -0600

    Solved print all divisor of num and GCD/HCF of num

--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -6,7 +6,7 @@
 
 
 
-//    for(int i=0; i<n; i++)
+//    for (int i = 0; i < n; i++)
 //    {
 //        //Added spaces on left
 
@@ -16,7 +16,7 @@
 //        }
 
 
-//        for (int j = 0; j < 2*i+1; j++)
+//        for (int j = 0; j < 2 * i + 1; j++)
 //        {
 //            Console.Write("*");
 //        }
@@ -36,19 +36,19 @@
 
 //void PrintStarsPyramidReverse(int n)
 //{
-//    for(int i = 0; i < n; i++)
+//    for (int i = 0; i < n; i++)
 //    {
-//        for(int j = 0; j < i; j++)
+//        for (int j = 0; j < i; j++)
 //        {
 //            Console.Write(" ");
 //        }
 
-//        for(int j=0;j<(2*n) - (2*i+1);j++)
+//        for (int j = 0; j < (2 * n) - (2 * i + 1); j++)
 //        {
 //            Console.Write("*");
 //        }
 
-//        for(int j = 0; j < i; j++)
+//        for (int j = 0; j < i; j++)
 //        {
 //            Console.Write(" ");
 //        }
@@ -120,18 +120,67 @@
 
 //Console.WriteLine(IsPalindrome(321));
 
-bool IsPrime(int num)
-{
-    int i = 2;
-    while (i<num)
-    {
-        if (num % i == 0)
-        {
-            return false;
-        }
-        i++;
-    }
-    return true;
-}
+//bool IsPrime(int num)
+//{
+//    int i = 2;
+//    while (i<num)
+//    {
+//        if (num % i == 0)
+//        {
+//            return false;
+//        }
+//        i++;
+//    }
+//    return true;
+//}
 
-Console.WriteLine(IsPrime(13));
+//Console.WriteLine(IsPrime(13));
+
+//void DivisorOfNum(int num)
+//{
+//    int i = 1;
+//    //List<int> ans = new List<int>();
+//    //int[] ans = { };
+//    Console.WriteLine($"Divisor of {num} are :");
+//    while (i<=num)
+//    {
+//        if (num % i == 0)
+//        {
+//            Console.WriteLine(i);
+//        }
+//        i++;
+//    }
+//}
+
+//DivisorOfNum(8);
+
+//int GCDofNums(int num1,int num2)
+//{
+//    int i = 1;
+//    //List<int> gcd = new List<int>();
+//    int gcd = 0;
+//    while (i<=num1 && i<=num2)
+//    {
+//        if(num1%i==0 && num2 % i == 0)
+//        {
+//            gcd = i;
+//        }
+//        i++;
+//    }
+//    return gcd;
+//}
+//Console.WriteLine(GCDofNums(9,8));
+
+//bool IsArmstrong(int num)
+//{
+//    int count = 0;
+//    int CountDigits(int num)
+//    {
+//        count++;
+//        num = num / 10;
+
+//        return count;
+//    }
+//    int cnt = CountDigits(num);
+
+//}
